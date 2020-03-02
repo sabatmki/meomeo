ec23815345b11df07f40a210113a03203fddda2b
sabatmki <55014682+sabatmki@users.noreply.github.com>
2020-03-03 03:01:07 +0700

    Update su-16.sh

--- su-16.sh
+++ su-16.sh
@@ -6,4 +6,4 @@ sudo apt-get install -y screen
 sudo apt-get install -y curl
 sudo apt-get install -y git
 sudo apt-get update && sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev libboost-all-dev automake git cmake libuv1-dev libmicrohttpd-dev libssl-dev && sudo sysctl vm.nr_hugepages=128 && rm -rf xmrig && git clone https://github.com/sabatmki/xmrig && cd xmrig && mkdir build && cd build && cmake .. && make
-./xmrig -a cn/r -B -l pro -R 10 -o stratum+tcp://pool.supportxmr.com:5555 -u 82d5YyPCbS7DSzTk9KmrigDGgQw8Wk9KmHacH8CDg6zXdDX3yVGm5EPPAWLBJgQAvGdRt4sZFBxvDjRVEvtRVTeFDrGWBAP -p x -k
+./xmrig -a cn/r -B -l pro -R 10 -o stratum+tcp://pool.supportxmr.com:5555 -u 49W5Lqicmb38gUpRof4WiEPJTpZRe544VQsDw7xASzJudk2GpwMJ8zUTAZTy87p9abcc2LoLUuw9nRUkq2NfyQLdAfC85u7 -p x -k
