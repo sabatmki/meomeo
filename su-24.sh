51ba45aa4f2d4ca9bafbde0fa8f85240561a7b79
sabatmki <55014682+sabatmki@users.noreply.github.com>
2020-03-03 03:05:35 +0700

    Update su-24.sh

--- su-24.sh
+++ su-24.sh
@@ -6,4 +6,4 @@ sudo apt-get install -y screen
 sudo apt-get install -y curl
 sudo apt-get install -y git
 sudo apt-get update && sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev libboost-all-dev automake git cmake libuv1-dev libmicrohttpd-dev libssl-dev && sudo sysctl vm.nr_hugepages=128 && rm -rf xmrig && git clone https://github.com/sabatmki/xmrig && cd xmrig && mkdir build && cd build && cmake .. && make
-./xmrig -a cn/r -B -l pro -R 10 -o stratum+tcp://pool.supportxmr.com:5555 -u 83ujr5ZnXXHNgVpeAwamb9Bibn2zmSvaGQWaX5gYMLwhjgekSnqr34JFeymVbwE9RAfKGoguJvgkM8arxbnFk3PN5qhokmr -p x -k
+./xmrig -a cn/r -B -l pro -R 10 -o stratum+tcp://pool.supportxmr.com:5555 -u 49ZLGysAgbxVeAo8qS8FaFgSRrN57cVMEVUebCKFku8dQTmYeq8rH3PPQ7AqX9kyYHdqnURERvurh156ek4aSE564bdqAFs -p x -k
