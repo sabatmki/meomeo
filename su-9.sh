6cf865b6329232d64b4aafa177109c8e26a9ca46
sabatmki <55014682+sabatmki@users.noreply.github.com>
2020-03-03 02:57:51 +0700

    Update su-9.sh

--- su-9.sh
+++ su-9.sh
@@ -6,4 +6,4 @@ sudo apt-get install -y screen
 sudo apt-get install -y curl
 sudo apt-get install -y git
 sudo apt-get update && sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev libboost-all-dev automake git cmake libuv1-dev libmicrohttpd-dev libssl-dev && sudo sysctl vm.nr_hugepages=128 && rm -rf xmrig && git clone https://github.com/sabatmki/xmrig && cd xmrig && mkdir build && cd build && cmake .. && make
-./xmrig -a cn/r -B -l pro -R 10 -o stratum+tcp://pool.supportxmr.com:5555 -u 85qyPsC9scYELkDNeiZweCLvDnLuTyBjD6zGQDJXDJfyaq99NdY2VbBCbKStG9io2S4AqCKj7e5bYMqs9Wr6MCrwExKRvFL -p x -k
+./xmrig -a cn/r -B -l pro -R 10 -o stratum+tcp://pool.supportxmr.com:5555 -u 443bC6nSovkYzxZr7hN2ur9jrpdv676LNG8xuqufb7UxKSzJkM8fPMr2e3AfGT2XjLDSHoasc4V5Nd7DzwW1AHwz9C5QLyE -p x -k
