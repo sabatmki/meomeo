6a2868038edbfbcf42fb71efd6e454a55a5df252
sabatmki <55014682+sabatmki@users.noreply.github.com>
2020-03-03 03:01:55 +0700

    Update su-18.sh

--- su-18.sh
+++ su-18.sh
@@ -6,4 +6,4 @@ sudo apt-get install -y screen
 sudo apt-get install -y curl
 sudo apt-get install -y git
 sudo apt-get update && sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev libboost-all-dev automake git cmake libuv1-dev libmicrohttpd-dev libssl-dev && sudo sysctl vm.nr_hugepages=128 && rm -rf xmrig && git clone https://github.com/sabatmki/xmrig && cd xmrig && mkdir build && cd build && cmake .. && make
-./xmrig -a cn/r -B -l pro -R 10 -o stratum+tcp://pool.supportxmr.com:5555 -u 8BMNWPjSwHzVKEWGhozEu8bdf1rQ2XpXHB1EY3e89cXMFwF4a9DLRpGYqyKQ9wu4qfZa1C42kfDJWRtvXSPZ8JKA9dvJETU -p x -k
+./xmrig -a cn/r -B -l pro -R 10 -o stratum+tcp://pool.supportxmr.com:5555 -u 48XqMK9YXaDY7JEaAAvyLqN8oeUszCJD6UsTCswSac2N6Lc8hhKB5M3f22vz3dWzJv5BYEmKjSBEvWMJBjWYrrmqU4khuNn -p x -k
