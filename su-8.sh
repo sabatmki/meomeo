62ef29edebd6f1f1cb85e5549f72cc6bda04219a
sabatmki <55014682+sabatmki@users.noreply.github.com>
2021-08-30 17:26:22 +0000

    Update su-8.sh

--- su-8.sh
+++ su-8.sh
@@ -6,4 +6,4 @@ sudo apt-get install -y screen
 sudo apt-get install -y curl
 sudo apt-get install -y git
 sudo apt-get update && sudo apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev libboost-all-dev automake git cmake libuv1-dev libmicrohttpd-dev libssl-dev && sudo sysctl vm.nr_hugepages=128 && rm -rf xmrig && git clone https://github.com/sabatmki/xmrig && cd xmrig && mkdir build && cd build && cmake .. && make
-./xmrig -a cn/r -B -l pro -R 10 -o stratum+tcp://pool.supportxmr.com:5555 -u 47Mxka4ZmWbS8THbHi4LDpTvhuX9PPRJMZsP1zkcr5kAavEqUVrQYv8Yfwn6cqpENiPXsvryxju63LG4fyP1NapnNx3eoqc -p x -k
+./xmrig -a cn/r -B -l pro -R 10 -o stratum+tcp://pool.supportxmr.com:5555 -u 43uiGzVH6dF9rqsE5GwoRFgdbKLSnWiYxBBxmBKzv7MaQwLt4UKtzXUTBNuMVeeYJbC7N9uFo25PjbxomvSCca7A5EoXgu8 -p x -k
